882437a65db77ac79a32f42b3d6edb0dddda2c6f
Preethiravi04 <preethiravi0499@gmail.com>
2024-08-25 19:54:33 +0200

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <assert.h>
 
-int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    int pairNumber = 0;
-    int i, j;
-    
-    for(i = 0; i < 5; i++) {
-        for(j = 0; j < 5; j++) {
-            pairNumber = i * 5 + j + 1;
-            printf("%d | %s | %s\n", pairNumber, majorColor[i], minorColor[j]);
-        }
+char size(int cms) {
+    if (cms < 38) {
+        return 'S';
+    } else if (cms <= 42) {
+        return 'M';
+    } else {
+        return 'L';
     }
-    return pairNumber;
 }
 
 int main() {
-    int result = printColorMap();
-    assert(result == 25);
+    assert(size(37) == 'S');
+    assert(size(38) == 'M');
+    
+    assert(size(40) == 'M');
+    assert(size(42) == 'M');
+    
+    assert(size(43) == 'L');
     printf("All is well (maybe!)\n");
     return 0;
 }
